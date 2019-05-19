ad9fdf124dc2255d576ed5c091477cc92e7ddcef
Costas Kiteou <ckiteou22@gmail.com>
2019-05-19 16:04:44 +0100

    Updated outdated test snapshots

--- tests/Navbar.test.js
+++ tests/Navbar.test.js
@@ -1,22 +1,12 @@
 import React from 'react';
 import renderer from 'react-test-renderer'
 
-import Navbar from '../src/components/home/Navbar';
-import { ContactDetails } from '../src/components/home/Navbar';
+import Navbar from '../src/components/Navbar';
 
 describe('Navbar', () => {
   it('renders', () => {
     const tree = renderer.create(
       <Navbar />
-    ).toJSON();
-    expect(tree).toMatchSnapshot()
-  });
-});
-
-describe('ContactDetails', () => {
-  it('renders', () => {
-    const tree = renderer.create(
-      <ContactDetails />
     ).toJSON();
     expect(tree).toMatchSnapshot()
   });
