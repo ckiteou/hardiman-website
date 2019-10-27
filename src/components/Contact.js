27d52b88902a1a49d81001163c354bbc43a72877
Costas Kiteou <ckiteou22@gmail.com>
2019-10-27 19:25:44 +0000

    Add email functionality

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -11,17 +11,109 @@ const ContactDetails = () => (
   </div>
 )
 
-const EmailForm = () => (
-  <div className="email_form">
-    <form>
-      <input type="text" name="name" Placeholder="Full Name" required/>
-      <input type="text" name="email" placeholder="Email Address" required/>
-      <input type="text" name="telephone" placeholder="Contact Number" required/>
-      <textarea type="text" name="details" placeholder="Message (please include location)" required/>
-      <input type="submit" value="Submit" className="submit"/>
-    </form>
-  </div>
-)
+class EmailForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      email: '',
+      telephone: '',
+      message: ''
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+    this.sendEmail = this.sendEmail.bind(this);
+    this.checkIfFieldsAreEmpty = this.checkIfFieldsAreEmpty.bind(this);
+  }
+  handleChange(e) {
+    const target = e.target;
+    const value = target.value;
+    const name = target.name
+    this.setState({[name]: value})
+  }
+  checkIfFieldsAreEmpty() {
+    let state = this.state;
+    return state.name !== '' && state.email !== '' && state.telephone !== '' && state.message !== ''
+  }
+  onSubmit(e) {
+    if (this.checkIfFieldsAreEmpty()) {
+      e.preventDefault();
+      const templateId = 'jmhgardens';
+      this.sendEmail(
+        templateId,
+        {
+          message: this.state.message,
+          name: this.state.name,
+          email: this.state.email,
+          telephone: this.state.telephone
+        }
+      )
+    }
+  }
+  sendEmail (templateId, variables) {
+  	window.emailjs.send(
+    	'gmail',
+      templateId,
+    	variables
+  	).then(res => {
+    	console.log('Email successfully sent!')
+  	}).then(() =>
+      this.setState({
+        name: '',
+        email: '',
+        telephone: '',
+        message: ''
+      })
+    )
+  	.catch(err => console.error("Balls, email didn't send!! Error:", err))
+  }
+  render() {
+    return (
+      <div className="email_form">
+        <form>
+          <input
+            type="text"
+            name="name"
+            value={this.state.name}
+            onChange={this.handleChange}
+            placeholder="Full Name"
+            required
+          />
+          <input
+            type="text"
+            name="email"
+            value={this.state.email}
+            onChange={this.handleChange}
+            placeholder="Email Address"
+            required
+          />
+          <input
+            type="text"
+            name="telephone"
+            value={this.state.telephone}
+            onChange={this.handleChange}
+            placeholder="Contact Number"
+            required
+          />
+          <textarea
+            type="text"
+            name="message"
+            value={this.state.message}
+            onChange={this.handleChange}
+            placeholder="Message (please include location)"
+            required
+          />
+          <input
+            type="submit"
+            value="Submit"
+            className="submit"
+            onClick={this.onSubmit}
+          />
+        </form>
+      </div>
+    )
+  }
+}
 
 const Contact = () => (
   <div className="contact_wrapper">
