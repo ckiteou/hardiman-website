6c5c1ae2ca0e7a9c1194f15c5705f39b63fa9d43
Costas Kiteou <costaskiteou@dsmac-00004.local>
2019-11-29 13:50:35 +0000

    Removed Carousel test - for now

--- src/data/home.js
+++ src/data/home.js
@@ -4,7 +4,7 @@ That's why for me it's important to build a relationship with my clients, to ena
 export const about_me =
 `"I have been running JMH Gardens since am a private gardner have been working as a gardener since 2009.\n
 I thoroughly enjoy creating new visions and etc\n
-Some more text abotu what I do and who I am. I'm a great gardener and love vodka. Seriously. Occasionally I drink too much (sad face). I need write more stuff so I can filll in the blanks ansd se what it looks like.\n
+Some more text about what I do and who I am. I'm a great gardener and love vodka. Seriously. Totally love it. I need write more stuff so I can filll in the blanks ansd se what it looks like.\n
 I'm going to Australia for christmas, with my fiance and little baby bo. His name is Tadg, pronounced Tige, like in tiger. He is the cutest lttlething ever!!!\n
 Anyway, I need to write more stuff to fill out the blanks, just not too sure what tot write. I coudl write about my Saturday today, spent the morning flying. Then cooked breakfast. Poached eggs on toast. The bread from Morrisons is rubbish I need to make my own tommorrow.\n
 So this is the last paragraph, I just need to write a few more lines and I am done/ So what shall I write, well, I just saw Robert De Niro jerking off in a Zac Efron film. Awesome. Scared for life! Seriously, who writes this shit?"`
